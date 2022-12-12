4aee261588862a362c7112e27beed94c9cfec163
Sirasit Sudnakrob <sirasit.sudnakrob@seagate.com>
2022-12-12 15:10:58 +0700

    Lambda Use Case

--- 03 Functions/lambdas_start.py
+++ 03 Functions/lambdas_start.py
@@ -14,8 +14,12 @@ def main():
     ftemps = [32, 65, 100, 212]
 
     # TODO: Use regular functions to convert temps
+    print("Celsisus Temp =", list(map(FahrenheitToCelsisus, ftemps)))
+    print("Fahrenheit Temp =", list(map(CelsisusToFahrenheit, ctemps)))
 
     # TODO: Use lambdas to accomplish the same thing
+    print("Celsisus Temp =", list(map(lambda t: (t-32) * 5/9, ftemps)))
+    print("Fahrenheit Temp =", list(map(lambda t: (t * 9/5) + 32, ctemps)))
 
 
 if __name__ == "__main__":
