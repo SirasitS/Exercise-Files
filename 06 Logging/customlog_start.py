63cf8d0e9112142fff37cebf582f67765e41c9ad
Sirasit Sudnakrob <sirasit.sudnakrob@seagate.com>
2022-12-15 11:02:19 +0700

    Custom logging use case

--- 06 Logging/customlog_start.py
+++ 06 Logging/customlog_start.py
@@ -2,16 +2,29 @@
 
 import logging
 
+extData = {
+    'user':'joem@example.com'
+}
+
 # TODO: add another function to log from
+def anotherFunction():
+    logging.debug("This is a debug-level message", extra=extData)
 
 def main():
     # set the output file and debug level, and
     # TODO: use a custom formatting specification
-    logging.basicConfig(filename="output.log",
-                        level=logging.DEBUG)
+    fmtstr = "User:%(user)s %(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d %(message)s"
+    datestr = "%m/%d/%Y %I:%M:%S %p"
 
-    logging.info("This is an info-level log message")
-    logging.warning("This is a warning-level message")
+    logging.basicConfig(filename="output.log",
+                        level=logging.DEBUG,
+                        filemode="w",
+                        format=fmtstr,
+                        datefmt=datestr)
+
+    logging.info("This is an info-level log message", extra=extData)
+    logging.warning("This is a warning-level message", extra=extData)
+    anotherFunction()
 
 
 if __name__ == "__main__":
